fe0ce34ce0dfc4aa0b4c2a6e1f9e27cbc9bd9394
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-29 22:14:16 -0400

    initial

---
+++ src/locha-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const locha = require('.')
+
+describe('locha', () => {
+  it('write this test', () => {
+    console.assert(locha, 'should export something')
+  })
+})
